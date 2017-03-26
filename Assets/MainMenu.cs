65164b0c823a936b03a5143cd924879500b53ecf
Evaldas <xevalxdasx8@gmail.com>
2017-03-26 18:22:28 +0300

    menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+
+
+    public string levelToLoad = "MainLevel";
+   public void Play()
+    {
+        SceneManager.LoadScene("MainLevel");
+    }
+    public void Quit()
+    {
+        Debug.Log("Exciting...");
+        Application.Quit();
+    }
+}
